5f18c135907b5d62ab7d3dbb8bd6f4b0cd954495
Alessandra Wong <awong@pdx.edu>
2023-01-17 10:54:30 -0800

    Committing last changes to prev quarter examples

--- javascript/exercises/02-format.js
+++ javascript/exercises/02-format.js
@@ -1,10 +1,15 @@
 const formatItems = (input) => {
   // return a string of the concatenated numbers, separated by commas and with three digits after the decimal point
 
+  let results = [];
+  for (const element of input) {
+    results.push(element.toFixed(3));
+  }
+
   // for reference, see the MDN Number page:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
 
-  return input;
+  return results.join(", ");
 };
 
 console.log(formatItems([1, 4, 17, 26, 41]));
